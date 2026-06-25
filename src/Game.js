b79aadccd8c0167727d377ea50cfead8267baf51
Gazen27 <gianni.adamo@hotmail.it>
2026-06-26 00:03:36 +0200

    implemented working movement

--- src/Game.js
+++ src/Game.js
@@ -14,14 +14,23 @@ export class Game {
     this.board.drawSnake(this.snakeBody);
   }
 
+  //Getting new direction
+  getDirection(event) {
+  if (event.key === "ArrowRight" && this.direction != "left") {this.direction = "right";}
+  if (event.key === "ArrowLeft" && this.direction != "right") {this.direction = "left";}
+  if (event.key === "ArrowUp" && this.direction != "down") {this.direction = "up";}
+  if (event.key === "ArrowDown" && this.direction != "up") {this.direction = "down";}
+  }
+
   //Starting the game
   start() {
+    document.addEventListener("keydown", this.getDirection.bind(this));
     this.interval = setInterval(this.update.bind(this), 120);
   }
 
   //Refreshing game instance
   update() {
-    this.snakeBody = this.snake.move("right");
+    this.snakeBody = this.snake.move(this.direction);
     this.board.clearSnake();
     this.board.drawSnake(this.snakeBody);
   }
